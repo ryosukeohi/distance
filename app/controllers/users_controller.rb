44a09f4d689b814d7bb82861eff4437b427c2d5e
ryosukeohi <ryosuke.ohi@gmail.com>
2022-03-11 10:30:57 +0000

    [Add]ランキング機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = Record.where(start_time: Time.current.all_month).order('sum(distance) desc').pluck(:user_id)
   end
 
   def show
     @user = User.find(params[:id])
     @records = current_user.records
-    @month = params[:month] ? Date.parse(params[month]) : Time.zone.today
-    @distances = Record.where(created_at: @month.all_month).where(user_id: current_user.id)
+    @distances = Record.where(start_time: Time.current.all_month).where(user_id: current_user.id)
     @total = @distances.all.sum('distance')
   end
 
