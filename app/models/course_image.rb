abb3da68006ba65f8970860bc452812c1a49560b
ryosukeohi <ryosuke.ohi@gmail.com>
2022-03-19 09:32:18 +0000

    [Fix]コース一覧画面(トップ)レイアウト修正

--- app/models/course_image.rb
+++ app/models/course_image.rb
@@ -2,6 +2,6 @@ class CourseImage < ApplicationRecord
   belongs_to :course
   attachment :image
 
-  validates :course_id, presence: true
-  validates :image_id, presence: true
+  # validates :course_id, presence: true
+  # validates :image_id, presence: true
 end
