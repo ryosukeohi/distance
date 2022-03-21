b2fe4c18f238b23fd8d00047870b4205d5d93715
ryosukeohi <ryosuke.ohi@gmail.com>
2022-03-21 23:34:32 +0000

    [Add]記録投稿画面テスト

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :course do
     title { Faker::Lorem.characters(number: 20) }
-    description { Faker::Lorem.characters(number: 300) }
+    description { Faker::Lorem.characters(number: 50) }
     distance { Faker::Lorem.characters(number: 6) }
     latitude { Faker::Lorem.characters(number: 10) }
     longitude { Faker::Lorem.characters(number: 10) }
     address { Faker::Lorem.characters(number: 20) }
+    user
   end
 end
--- spec/system/recourd_spec.rb
+++ spec/system/recourd_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe '投稿のテスト' do
-  let(:course) {create(:course, user: user)}
-  let(:course2) {create(:course, user: user2)}
+  let(:record) {create(:record, user: user)}
+  let(:record2) {create(:record, user: user2)}
   let(:user) {create(:user)}
   let(:user2) {create(:user)}
   before do
@@ -12,12 +12,12 @@ describe '投稿のテスト' do
     visit courses_path
   end
   describe '表示テスト' do
-    context 'コース投稿画面' do
+    context '記録投稿画面' do
       before do
-        visit new_course_path(user, course)
+        visit new_record_path(user, record)
       end
-      it 'titleフォームが表示される' do
-        expect(page).to have_field 'course[title]'
+      it 'distanceフォームが表示される' do
+        expect(page).to have_field 'record[distance]'
       end
       it '投稿ボタンが表示される' do
         expect(page).to have_button '投稿'
@@ -25,15 +25,15 @@ describe '投稿のテスト' do
     end
   end
   describe '編集のテスト' do
-    context '自分のコースの編集画面に遷移' do
+    context '自分の記録の編集画面に遷移' do
       it '遷移できる' do
-        visit edit_course_path(course)
-        expect(current_path).to eq('/courses/' + course.id.to_s + '/edit')
+        visit edit_record_path(record)
+        expect(current_path).to eq('/records/' + record.id.to_s + '/edit')
       end
     end
     context '他ユーザーの編集画面に遷移' do
       it '遷移できない' do
-        visit edit_course_path(course2)
+        visit edit_record_path(record2)
         expect(current_path).to eq('/users/' + user.id.to_s)
       end
     end
