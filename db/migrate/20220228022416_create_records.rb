6d25e5c109716140cd804a20bba52fa1a84cdf2a
ryosukeohi <ryosuke.ohi@gmail.com>
2022-03-26 11:15:59 +0000

    [Fix]recordsテーブルマイグレーションファイル修正

--- db/migrate/20220228022416_create_records.rb
+++ db/migrate/20220228022416_create_records.rb
@@ -2,7 +2,7 @@ class CreateRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :records do |t|
       t.integer :user_id, null: false
-      t.text :description, null: false, default: ""
+      t.text :description
       t.float :distance, null: false
 
       t.timestamps null: false
