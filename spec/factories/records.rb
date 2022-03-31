508704200c3fc1cfd9b03cdd7262c886f595ca5b
ryosukeohi <ryosuke.ohi@gmail.com>
2022-03-31 13:37:02 +0000

    [Fix]結合テスト

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :record do
     distance {10}
+    description {100}
     user_id {3}
     start_time {'2022/01/01'}
   end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,11 +1,35 @@
 require 'rails_helper'
 
-Rspec.describe 'ユーザー新規登録', type: :system do
-  visit root_path
-  click_on "新規登録"
-  fill_in "name", with: "テスト"
-  fill_in "email", with: "test@test"
-  fill_in "password", "111111"
-  fill_in "password", "111111"
-  click_on "Si"
+RSpec.describe '投稿のテスト', type: :system do
+  before do
+   @user = FactoryBot.build(:user)
+  end
+  describe "ユーザー新規登録" do
+    it "ユーザー新規登録ができるか" do
+      visit root_path
+      click_on "新規登録"
+      fill_in "Name", with: @user.name
+      fill_in "Email", with: @user.email
+      fill_in "Password", with: @user.password
+      fill_in "Password confirmation", with: @user.password
+      click_on "Sign up"
+      expect(current_path).to eq('/courses')
+    end
+  end
+  describe "記録投稿" do
+    before do
+      @record = FactoryBot.build(:record)
+    end
+    context "正常に記録を投稿できるか" do
+      it "投稿詳細に遷移するか" do
+        click_on "マイページ"
+        click_on "マイページ"
+        fill_in "description", with: @course.description
+        fill_in "distance", with: @course.distance
+        fill_in "start_time", with: @course.start_time
+        click_on "投稿"
+        expect(current_path).to eq('/courses/' + @course.id.to_s)
+       end
+    end
+  end
 end
