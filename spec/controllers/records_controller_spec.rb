a8c02028a8521c6e06142134dc2fca52c44a86ba
ryosukeohi <ryosuke.ohi@gmail.com>
2022-04-17 04:02:11 +0000

    [Fix]Record保存のテスト

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
-RSpec.describe 'RecordsController', type: :controller do
+RSpec.describe RecordsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:record) { create(:record) }
   context 'POST #create' do
-    before do
-      let(:user) { create(:user) }
-      let(:record) { create(:record, user_id: user.id) }
-    end
     it '正常に保存される' do
       expect {
-        post :create, record: attributes_for(:record)
+        post :create, params: { record: attributes_for(:record), user_id: user.id}
       }.to change(Record, :count).by(1)
     end
   end
